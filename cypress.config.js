bc93f5f44fc5af251717220a0c77cb81841a9db8
Kashifa Hasan <kashifahasan@kashifas-mini.myfiosgateway.com>
2023-12-20 22:35:09 -0500

    Adding Reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '14gif9',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    
   },
-  video: true,
 });
+
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   reporter: "reporters/custom.js",
+
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
